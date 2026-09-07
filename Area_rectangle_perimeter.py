163ebafd12f0bee096d9679fd29cdaae832c1852
SHIVAM RAY <raynavin20@gmail.com>
2026-09-07 20:24:39 +0530

    added Area_rectangle_perimeter.py file

---
+++ Area_rectangle_perimeter.py
@@ -0,0 +1,13 @@
+LENGTH = int(input("enter a number : "))
+BREADTH = int(input("enter a number : "))
+
+
+# area of rectangle = LENGTH * BREADTH
+# PERIMETER OF RECTANGLE = 2*(LENGTH + BREADTH)
+
+
+AREA = LENGTH * BREADTH
+PERIMETER = 2*(LENGTH + BREADTH)
+
+print("AREA OF RECTANGLE : ",AREA)
+print("PERIMETER OF RECTANGLE : ",PERIMETER)
