7156b02502ed374e271c6db31fdff956bfce2f3d
SHIVAM RAY <raynavin20@gmail.com>
2026-09-07 20:20:13 +0530

    added Airthmetic_operation.py file

---
+++ Airthmetic_operation.py
@@ -0,0 +1,15 @@
+a = int(input("enter a number : "))
+b = int(input("enter a number : "))
+
+
+sum = a+b
+substract = a-b
+multiply = a*b
+divide = a/b
+mod = a%b
+
+print("sum of two no = ",sum)
+print("substract of two no = ",substract)
+print("multiply of two no = ",multiply)
+print("divide of two no = ",divide)
+print("mod of two no = ",mod)
