f005e7afcc40cd51b6278afdedfa820e78f59ef0
SHIVAM RAY <raynavin20@gmail.com>
2026-09-10 23:45:40 +0530

    Added python practice files

---
+++ set_operations.py
@@ -0,0 +1,5 @@
+group_a = {"Python", "C", "HTML"}
+group_b = {"C", "JavaScript", "Python"}
+
+print("Sabhi subjects (Union):", group_a.union(group_b))
+print("Common subjects (Intersection):", group_a.intersection(group_b))
