27ad84bab7b57d4bb5399b78bcf312452a48b0c0
SHIVAM RAY <raynavin20@gmail.com>
2026-09-10 23:42:14 +0530

    Added python practice files

---
+++ data_structures_combined.py
@@ -0,0 +1,8 @@
+users = [
+    {"name": "Shivam", "tech_stack": {"Python", "C"}},
+    {"name": "Aman", "tech_stack": {"HTML", "CSS", "Python"}}
+]
+
+for user in users:
+    print("Naam:", user["name"])
+    print("Skills:", user["tech_stack"])
