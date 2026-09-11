513ede680c3ef4f1d897f2bc1adbb1fc5274d868
SHIVAM RAY <raynavin20@gmail.com>
2026-09-11 18:41:43 +0530

    Added python practice files

---
+++ list_calculations.py
@@ -0,0 +1,6 @@
+marks = [78, 92, 65, 88, 54]
+
+print("Sabse zyada marks:", max(marks))
+print("Sabse kam marks:", min(marks))
+print("Total marks:", sum(marks))
+print("Average marks:", sum(marks) / len(marks))
