7fac7f1afd94a430978bf396e7882c3bc7492277
SHIVAM RAY <raynavin20@gmail.com>
2026-09-16 19:44:52 +0530

    Add python Programming practice files

---
+++ pattern_right_triangle.py
@@ -0,0 +1,9 @@
+num = int(input("enter the number : "))
+
+for i in range(1,num+1):
+   for j in range(1, num+1):
+      if i + j > num:
+       print("*",end="")
+      else:
+       print(" ",end="") 
+   print()
