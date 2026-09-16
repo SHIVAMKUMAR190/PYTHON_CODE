654d4a7626ac02b5154a0b7a603160955410da7e
SHIVAM RAY <raynavin20@gmail.com>
2026-09-16 19:36:52 +0530

    Add python Programming practice files

---
+++ pattern_reverse_stars.py
@@ -0,0 +1,9 @@
+num = int(input("enter the number : "))
+
+for i in range(1,num+1):
+   for j in range(1, num+1):
+      if j>=i:
+       print("*",end="")
+      else:
+       print(" ",end="") 
+   print()
