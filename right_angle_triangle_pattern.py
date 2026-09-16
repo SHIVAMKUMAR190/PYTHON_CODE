69fcebb1570dff7019c1e93155d44e4ef8c62eec
SHIVAM RAY <raynavin20@gmail.com>
2026-09-16 19:41:27 +0530

    Add python Programming practice files

---
+++ right_angle_triangle_pattern.py
@@ -0,0 +1,15 @@
+
+# opposite right angle triangle
+
+num = int(input("enter the number : "));
+
+for i in range(num,0,-1):
+    print(" #@# " * i)
+
+
+# right angle triangle
+
+num = int(input("enter the number : "));
+
+for i in range(1,num+1):
+    print(" #@# " * i)
